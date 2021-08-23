6c24142b2383e8699700baed02f3dd1c8c910a25
Thomas Estupina <thomas.estupina@gmail.com>
2021-08-23 17:27:00 +0200

    ajout du publicpath

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,7 @@
+const publicPath = process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
+module.exports = {
+ publicPath: publicPath,
+}
 module.exports = {
     chainWebpack: config => {
       config.module
