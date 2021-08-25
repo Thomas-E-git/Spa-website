be190445e078b470cbf0f12af1bda9494d654a79
Thomas Estupina <thomas.estupina@gmail.com>
2021-08-25 14:12:47 +0200

    ajout de dist

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const publicPath = process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
 module.exports = {
  publicPath: publicPath,
 }
-//test
 module.exports = {
     chainWebpack: config => {
       config.module
